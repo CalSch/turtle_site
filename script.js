5074791defead3b82437c7fa98dd2c7466f93356
CalSch <35741152+CalSch@users.noreply.github.com>
2021-10-14 08:28:17 -0500

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 try {
     let c=document.getElementById("game");
-    c.width=100;
-    c.height=100;
+    c.width=500;
+    c.height=500;
     let ctx=c.getContext('2d');
     let x=10;
     let y=10;
@@ -25,12 +25,17 @@ try {
     
     function draw() {
         ctx.fillStyle="white";
-        ctx.clearRect(0,0,100,100);
+        ctx.clearRect(0,0,500,500);
         ctx.fillStyle="red";
         ctx.arc(x,y,5,0,2*Math.PI);
         ctx.fill();
+
+        ctx.strokeStyle="2px black";
         ctx.moveTo(x,y);
+        ctx.beginPath();
         ctx.lineTo(x+Math.cos(deg2rad(theta))*5,y+Math.sin(deg2rad(theta))*5);
+        ctx.closePath();
+        ctx.stroke();
     }
     function update() {
 
