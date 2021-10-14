d0d6d4f7bef79d2d808971c1e43f94bf63feab9d
CalSch <35741152+CalSch@users.noreply.github.com>
2021-10-14 08:09:30 -0500

    updated

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 try {
     let c=document.getElementById("game");
+    c.width=100;
+    c.height=100;
     let ctx=c.getContext('2d');
+    let x=10;
+    let y=10;
+    let theta=0;
+
+    function fd(pix) {
+        x+=Math.cos(theta)*pix;
+        y+=Math.sin(theta)*pix;
+    }
     
-    ctx.fillStyle="red";
-    ctx.fillRect(0,0,10,10);
+    function draw() {
+        ctx.fillStyle="white";
+        ctx.clearRect(0,0,100,100);
+        ctx.fillStyle="red";
+        ctx.fillRect(x,y,10,10);
+    }
+    function update() {
+
+    }
+    setInterval(function() {
+        update();
+        draw();
+    },1000/60)
 } catch(e) {
     alert("error: "+e)
 }
